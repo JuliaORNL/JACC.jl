cf6c9f51826165a2ae4b98e291dcbd806f873ccd
William F Godoy <williamfgc@yahoo.com>
2024-02-05 09:58:48 -0500

    Apply JuliaFormatter

--- ext/JACCAMDGPU/JACCAMDGPU.jl
+++ ext/JACCAMDGPU/JACCAMDGPU.jl
@@ -6,28 +6,28 @@ function JACC.parallel_for(N::I, f::F, x...) where {I<:Integer,F<:Function}
   numThreads = 512
   threads = min(N, numThreads)
   blocks = ceil(Int, N / threads)
-  @roc groupsize=threads gridsize=threads*blocks _parallel_for_amdgpu(f, x...)
+  @roc groupsize = threads gridsize = threads * blocks _parallel_for_amdgpu(f, x...)
   #AMDGPU.synchronize()
 end
 
 function JACC.parallel_for((M, N), f::F, x...) where {F<:Function}
-  numThreads = 16 
+  numThreads = 16
   Mthreads = min(M, numThreads)
-  Nthreads = min(N, numThreads)  
-  Mblocks = ceil(Int, M/Mthreads)
-  Nblocks = ceil(Int, N/Nthreads)
-  @roc groupsize=(Mthreads, Nthreads) gridsize=(Mblocks*Mthreads, Nblocks*Nthreads) _parallel_for_amdgpu_MN(f, x...)
+  Nthreads = min(N, numThreads)
+  Mblocks = ceil(Int, M / Mthreads)
+  Nblocks = ceil(Int, N / Nthreads)
+  @roc groupsize = (Mthreads, Nthreads) gridsize = (Mblocks * Mthreads, Nblocks * Nthreads) _parallel_for_amdgpu_MN(f, x...)
   AMDGPU.synchronize()
 end
 
 function JACC.parallel_reduce(N::I, f::F, x...) where {I<:Integer,F<:Function}
   numThreads = 512
   threads = min(N, numThreads)
-  blocks = ceil(Int, N/threads)
-  ret = AMDGPU.zeros(Float64,blocks)
-  rret = AMDGPU.zeros(Float64,1)
-  @roc groupsize=threads gridsize=threads*blocks localmem=512*sizeof(Float64) _parallel_reduce_amdgpu(N, ret, f, x...)
-  @roc groupsize=threads gridsize=threads localmem=512*sizeof(Float64) reduce_kernel_amdgpu(blocks, ret, rret)
+  blocks = ceil(Int, N / threads)
+  ret = AMDGPU.zeros(Float64, blocks)
+  rret = AMDGPU.zeros(Float64, 1)
+  @roc groupsize = threads gridsize = threads * blocks localmem = 512 * sizeof(Float64) _parallel_reduce_amdgpu(N, ret, f, x...)
+  @roc groupsize = threads gridsize = threads localmem = 512 * sizeof(Float64) reduce_kernel_amdgpu(blocks, ret, rret)
   return rret
 
 end
@@ -36,12 +36,12 @@ function JACC.parallel_reduce((M, N), f::F, x...) where {F<:Function}
   numThreads = 16
   Mthreads = min(M, numThreads)
   Nthreads = min(N, numThreads)
-  Mblocks = ceil(Int, M/Mthreads)
-  Nblocks = ceil(Int, N/Nthreads)
-  ret = AMDGPU.zeros(Float64,(Mblocks, Nblocks))
-  rret = AMDGPU.zeros(Float64,1)
-  @roc groupsize=(Mthreads, Nthreads) gridsize=(Mblocks*Mthreads, Nblocks*Nthreads) localmem=16*16*sizeof(Float64) _parallel_reduce_amdgpu_MN((M, N), ret, f, x...)
- @roc groupsize=(Mblocks, Nblocks) gridsize=(Mblocks, Nblocks) localmem=16*16*sizeof(Float64) reduce_kernel_amdgpu_MN((Mblocks, Nblocks), ret, rret) 
+  Mblocks = ceil(Int, M / Mthreads)
+  Nblocks = ceil(Int, N / Nthreads)
+  ret = AMDGPU.zeros(Float64, (Mblocks, Nblocks))
+  rret = AMDGPU.zeros(Float64, 1)
+  @roc groupsize = (Mthreads, Nthreads) gridsize = (Mblocks * Mthreads, Nblocks * Nthreads) localmem = 16 * 16 * sizeof(Float64) _parallel_reduce_amdgpu_MN((M, N), ret, f, x...)
+  @roc groupsize = (Mblocks, Nblocks) gridsize = (Mblocks, Nblocks) localmem = 16 * 16 * sizeof(Float64) reduce_kernel_amdgpu_MN((Mblocks, Nblocks), ret, rret)
   return rret
 end
 
@@ -71,7 +71,7 @@ function _parallel_reduce_amdgpu(N, ret, f, x...)
   end
   AMDGPU.sync_workgroup()
   if (ti <= 256)
-   shared_mem[ti] += shared_mem[ti+256]
+    shared_mem[ti] += shared_mem[ti+256]
   end
   AMDGPU.sync_workgroup()
   if (ti <= 128)
@@ -111,57 +111,57 @@ function _parallel_reduce_amdgpu(N, ret, f, x...)
 end
 
 function reduce_kernel_amdgpu(N, red, ret)
-    shared_mem = @ROCDynamicLocalArray(Float64, 512)
-    i = (workgroupIdx().x - 1) * workgroupDim().x + workitemIdx().x
-    ii = i
-    tmp::Float64 = 0.0
-    if N > 512
-      while ii <= N
-        tmp += @inbounds red[ii]
-        ii += 512
-      end
-    else
-      tmp = @inbounds red[i]
+  shared_mem = @ROCDynamicLocalArray(Float64, 512)
+  i = (workgroupIdx().x - 1) * workgroupDim().x + workitemIdx().x
+  ii = i
+  tmp::Float64 = 0.0
+  if N > 512
+    while ii <= N
+      tmp += @inbounds red[ii]
+      ii += 512
     end
-    shared_mem[i] = tmp
-    AMDGPU.sync_workgroup()
-    if (i <= 256)
-      shared_mem[i] += shared_mem[i+256]
-    end
-    AMDGPU.sync_workgroup()
-    if (i <= 128)
-      shared_mem[i] += shared_mem[i+128]
-    end
-    AMDGPU.sync_workgroup()
-    if (i <= 64)
-      shared_mem[i] += shared_mem[i+64]
-    end
-    AMDGPU.sync_workgroup()
-    if (i <= 32)
-      shared_mem[i] += shared_mem[i+32]
-    end
-    AMDGPU.sync_workgroup()
-    if (i <= 16)
-      shared_mem[i] += shared_mem[i+16]
-    end
-    AMDGPU.sync_workgroup()
-    if (i <= 8)
-      shared_mem[i] += shared_mem[i+8]
-    end
-    AMDGPU.sync_workgroup()
-    if (i <= 4)
-      shared_mem[i] += shared_mem[i+4]
-    end
-    AMDGPU.sync_workgroup()
-    if (i <= 2)
-      shared_mem[i] += shared_mem[i+2]
-    end
-    AMDGPU.sync_workgroup()
-    if (i == 1)
-      shared_mem[i] += shared_mem[i+1]
-      ret[1] = shared_mem[1]
-    end
-    return nothing
+  else
+    tmp = @inbounds red[i]
+  end
+  shared_mem[i] = tmp
+  AMDGPU.sync_workgroup()
+  if (i <= 256)
+    shared_mem[i] += shared_mem[i+256]
+  end
+  AMDGPU.sync_workgroup()
+  if (i <= 128)
+    shared_mem[i] += shared_mem[i+128]
+  end
+  AMDGPU.sync_workgroup()
+  if (i <= 64)
+    shared_mem[i] += shared_mem[i+64]
+  end
+  AMDGPU.sync_workgroup()
+  if (i <= 32)
+    shared_mem[i] += shared_mem[i+32]
+  end
+  AMDGPU.sync_workgroup()
+  if (i <= 16)
+    shared_mem[i] += shared_mem[i+16]
+  end
+  AMDGPU.sync_workgroup()
+  if (i <= 8)
+    shared_mem[i] += shared_mem[i+8]
+  end
+  AMDGPU.sync_workgroup()
+  if (i <= 4)
+    shared_mem[i] += shared_mem[i+4]
+  end
+  AMDGPU.sync_workgroup()
+  if (i <= 2)
+    shared_mem[i] += shared_mem[i+2]
+  end
+  AMDGPU.sync_workgroup()
+  if (i == 1)
+    shared_mem[i] += shared_mem[i+1]
+    ret[1] = shared_mem[1]
+  end
+  return nothing
 end
 
 function _parallel_reduce_amdgpu_MN((M, N), ret, f, x...)
@@ -181,29 +181,29 @@ function _parallel_reduce_amdgpu_MN((M, N), ret, f, x...)
     shared_mem[(ti-1)*16+tj] = tmp
   end
   AMDGPU.sync_workgroup()
-  if (ti <= 8 && tj <= 8 && ti+8 <= M && tj+8 <= N)
+  if (ti <= 8 && tj <= 8 && ti + 8 <= M && tj + 8 <= N)
     shared_mem[((ti-1)*16)+tj] += shared_mem[((ti+7)*16)+(tj+8)]
     shared_mem[((ti-1)*16)+tj] += shared_mem[((ti-1)*16)+(tj+8)]
     shared_mem[((ti-1)*16)+tj] += shared_mem[((ti+7)*16)+tj]
   end
   AMDGPU.sync_workgroup()
-  if (ti <= 4 && tj <= 4 && ti+4 <= M && tj+4 <= N)
+  if (ti <= 4 && tj <= 4 && ti + 4 <= M && tj + 4 <= N)
     shared_mem[((ti-1)*16)+tj] += shared_mem[((ti+3)*16)+(tj+4)]
     shared_mem[((ti-1)*16)+tj] += shared_mem[((ti-1)*16)+(tj+4)]
     shared_mem[((ti-1)*16)+tj] += shared_mem[((ti+3)*16)+tj]
   end
   AMDGPU.sync_workgroup()
-  if (ti <= 2 && tj <= 2 && ti+2 <= M && tj+2 <= N)
+  if (ti <= 2 && tj <= 2 && ti + 2 <= M && tj + 2 <= N)
     shared_mem[((ti-1)*16)+tj] += shared_mem[((ti+1)*16)+(tj+2)]
     shared_mem[((ti-1)*16)+tj] += shared_mem[((ti-1)*16)+(tj+2)]
     shared_mem[((ti-1)*16)+tj] += shared_mem[((ti+1)*16)+tj]
   end
   AMDGPU.sync_workgroup()
-  if (ti == 1 && tj == 1 && ti+1 <= M && tj+1 <= N)
+  if (ti == 1 && tj == 1 && ti + 1 <= M && tj + 1 <= N)
     shared_mem[((ti-1)*16)+tj] += shared_mem[ti*16+(tj+1)]
     shared_mem[((ti-1)*16)+tj] += shared_mem[((ti-1)*16)+(tj+1)]
     shared_mem[((ti-1)*16)+tj] += shared_mem[ti*16+tj]
-    ret[bi,bj] = shared_mem[((ti-1)*16)+tj]
+    ret[bi, bj] = shared_mem[((ti-1)*16)+tj]
   end
   return nothing
 end
@@ -217,86 +217,86 @@ function reduce_kernel_amdgpu_MN((M, N), red, ret)
 
   tmp::Float64 = 0.0
   shared_mem[(i-1)*16+j] = tmp
-  
+
   if M > 16 && N > 16
     while ii <= M
       jj = workitemIdx().y
       while jj <= N
-        tmp = tmp + @inbounds red[ii,jj]
+        tmp = tmp + @inbounds red[ii, jj]
         jj += 16
       end
       ii += 16
     end
   elseif M > 16
     while ii <= N
-      tmp = tmp + @inbounds red[ii,jj]
+      tmp = tmp + @inbounds red[ii, jj]
       ii += 16
     end
   elseif N > 16
     while jj <= N
-      tmp = tmp + @inbounds red[ii,jj]
+      tmp = tmp + @inbounds red[ii, jj]
       jj += 16
     end
   elseif M <= 16 && N <= 16
     if i <= M && j <= N
-      tmp = tmp + @inbounds red[i,j]
+      tmp = tmp + @inbounds red[i, j]
     end
   end
   shared_mem[(i-1)*16+j] = tmp
   AMDGPU.sync_workgroup()
   if (i <= 8 && j <= 8)
-    if (i+8 <= M && j+8 <= N)
+    if (i + 8 <= M && j + 8 <= N)
       shared_mem[((i-1)*16)+j] += shared_mem[((i+7)*16)+(j+8)]
     end
-    if (i <= M && j+8 <= N)
+    if (i <= M && j + 8 <= N)
       shared_mem[((i-1)*16)+j] += shared_mem[((i-1)*16)+(j+8)]
     end
-    if (i+8 <= M && j <= N)
+    if (i + 8 <= M && j <= N)
       shared_mem[((i-1)*16)+j] += shared_mem[((i+7)*16)+j]
     end
   end
   AMDGPU.sync_workgroup()
   if (i <= 4 && j <= 4)
-    if (i+4 <= M && j+4 <= N)
+    if (i + 4 <= M && j + 4 <= N)
       shared_mem[((i-1)*16)+j] += shared_mem[((i+3)*16)+(j+4)]
     end
-    if (i <= M && j+4 <= N)
+    if (i <= M && j + 4 <= N)
       shared_mem[((i-1)*16)+j] += shared_mem[((i-1)*16)+(j+4)]
     end
-    if (i+4 <= M && j <= N)
+    if (i + 4 <= M && j <= N)
       shared_mem[((i-1)*16)+j] += shared_mem[((i+3)*16)+j]
     end
   end
   AMDGPU.sync_workgroup()
   if (i <= 2 && j <= 2)
-    if (i+2 <= M && j+2 <= N)
+    if (i + 2 <= M && j + 2 <= N)
       shared_mem[((i-1)*16)+j] += shared_mem[((i+1)*16)+(j+2)]
     end
-    if (i <= M && j+2 <= N)
+    if (i <= M && j + 2 <= N)
       shared_mem[((i-1)*16)+j] += shared_mem[((i-1)*16)+(j+2)]
     end
-    if (i+2 <= M && j <= N)
+    if (i + 2 <= M && j <= N)
       shared_mem[((i-1)*16)+j] += shared_mem[((i+1)*16)+j]
     end
   end
   AMDGPU.sync_workgroup()
   if (i == 1 && j == 1)
-    if (i+1 <= M && j+1 <= N)
+    if (i + 1 <= M && j + 1 <= N)
       shared_mem[((i-1)*16)+j] += shared_mem[i*16+(j+1)]
     end
-    if (i <= M && j+1 <= N)
+    if (i <= M && j + 1 <= N)
       shared_mem[((i-1)*16)+j] += shared_mem[((i-1)*16)+(j+1)]
     end
-    if (i+1 <= M && j <= N)  
+    if (i + 1 <= M && j <= N)
       shared_mem[((i-1)*16)+j] += shared_mem[i*16+j]
     end
-    ret[1] = shared_mem[((i-1)*16)+j] 
+    ret[1] = shared_mem[((i-1)*16)+j]
   end
   return nothing
 end
 
 function __init__()
-    const JACC.Array = AMDGPU.ROCArray{T,N} where {T,N}
+  const JACC.Array = AMDGPU.ROCArray{T,N} where {T,N}
 end
 
 end # module JACCAMDGPU
--- test/tests_threads.jl
+++ test/tests_threads.jl
@@ -54,22 +54,22 @@ end
 
 @testset "CG" begin
 
-    function matvecmul(i,a1,a2,a3,x,y,SIZE)
+    function matvecmul(i, a1, a2, a3, x, y, SIZE)
         if i == 1
             y[i] = a2[i] * x[i] + a1[i] * x[i+1]
         elseif i == SIZE
             y[i] = a3[i] * x[i-1] + a2[i] * x[i]
         elseif i > 1 && i < SIZE
-            y[i] = a3[i] * x[i-1] + a1[i] * + x[i] + a1[i] * + x[i+1]
+            y[i] = a3[i] * x[i-1] + a1[i] * +x[i] + a1[i] * +x[i+1]
         end
     end
- 
-    function dot(i,x,y)
+
+    function dot(i, x, y)
         @inbounds return x[i] * y[i]
     end
 
     function axpy(i, alpha, x, y)
-        @inbounds x[i] += alpha[1,1] * y[i]
+        @inbounds x[i] += alpha[1, 1] * y[i]
     end
 
     SIZE = 10
@@ -87,36 +87,36 @@ end
     p = p * 0.5
     global cond = one(Float64)
 
-    while cond[1,1] >= 1e-14
+    while cond[1, 1] >= 1e-14
 
         r_old = copy(r)
 
-        JACC.parallel_for(SIZE,matvecmul,a0,a1,a2,p,s,SIZE)
+        JACC.parallel_for(SIZE, matvecmul, a0, a1, a2, p, s, SIZE)
 
-        alpha0 = JACC.parallel_reduce(SIZE,dot,r,r)
-        alpha1 = JACC.parallel_reduce(SIZE,dot,p,s)
+        alpha0 = JACC.parallel_reduce(SIZE, dot, r, r)
+        alpha1 = JACC.parallel_reduce(SIZE, dot, p, s)
 
         alpha = alpha0 / alpha1
         negative_alpha = alpha * (-1.0)
 
-        JACC.parallel_for(SIZE,axpy,negative_alpha,r,s)
-        JACC.parallel_for(SIZE,axpy,alpha,x,p)
+        JACC.parallel_for(SIZE, axpy, negative_alpha, r, s)
+        JACC.parallel_for(SIZE, axpy, alpha, x, p)
 
-        beta0 = JACC.parallel_reduce(SIZE,dot,r,r)
-        beta1 = JACC.parallel_reduce(SIZE,dot,r_old,r_old)
+        beta0 = JACC.parallel_reduce(SIZE, dot, r, r)
+        beta1 = JACC.parallel_reduce(SIZE, dot, r_old, r_old)
         beta = beta0 / beta1
 
         r_aux = copy(r)
 
-        JACC.parallel_for(SIZE,axpy,beta,r_aux,p)
-        ccond = JACC.parallel_reduce(SIZE,dot,r,r)
+        JACC.parallel_for(SIZE, axpy, beta, r_aux, p)
+        ccond = JACC.parallel_reduce(SIZE, dot, r, r)
         global cond = ccond
         p = copy(r_aux)
 
         println(cond)
 
     end
-    @test cond[1,1] <= 1e-14
+    @test cond[1, 1] <= 1e-14
 end
 
 @testset "LBM" begin
@@ -125,30 +125,30 @@ end
         u = 0.0
         v = 0.0
         p = 0.0
-        x_stream = 0 
+        x_stream = 0
         y_stream = 0
-  
+
         if x > 1 && x < SIZE && y > 1 && y < SIZE
             for k in 1:9
                 @inbounds x_stream = x - cx[k]
                 @inbounds y_stream = y - cy[k]
-                ind =  ( k - 1 ) * SIZE * SIZE + x * SIZE + y
-                iind = ( k - 1 ) * SIZE * SIZE + x_stream * SIZE + y_stream
-                @inbounds f[trunc(Int,ind)] = f1[trunc(Int,iind)] 
+                ind = (k - 1) * SIZE * SIZE + x * SIZE + y
+                iind = (k - 1) * SIZE * SIZE + x_stream * SIZE + y_stream
+                @inbounds f[trunc(Int, ind)] = f1[trunc(Int, iind)]
             end
             for k in 1:9
-                ind =  ( k - 1 ) * SIZE * SIZE + x * SIZE + y
-                @inbounds p = p[1,1] + f[ind]
-                @inbounds u = u[1,1] + f[ind] * cx[k]
-                @inbounds v = v[1,1] + f[ind] * cy[k]
+                ind = (k - 1) * SIZE * SIZE + x * SIZE + y
+                @inbounds p = p[1, 1] + f[ind]
+                @inbounds u = u[1, 1] + f[ind] * cx[k]
+                @inbounds v = v[1, 1] + f[ind] * cy[k]
             end
             u = u / p
             v = v / p
             for k in 1:9
-                @inbounds cu = cx[k] * u + cy[k] * v 
-                @inbounds feq = w[k] * p * ( 1.0 + 3.0 * cu + cu * cu - 1.5 * ( ( u * u ) + ( v * v ) ) )
-                ind =  ( k - 1 ) * SIZE * SIZE + x * SIZE + y
-                @inbounds f2[trunc(Int,ind)] = f[trunc(Int,ind)] * (1.0 - 1.0 / t) + feq * 1 / t
+                @inbounds cu = cx[k] * u + cy[k] * v
+                @inbounds feq = w[k] * p * (1.0 + 3.0 * cu + cu * cu - 1.5 * ((u * u) + (v * v)))
+                ind = (k - 1) * SIZE * SIZE + x * SIZE + y
+                @inbounds f2[trunc(Int, ind)] = f[trunc(Int, ind)] * (1.0 - 1.0 / t) + feq * 1 / t
             end
         end
     end
@@ -160,30 +160,30 @@ end
                 u = 0.0
                 v = 0.0
                 p = 0.0
-                x_stream = 0 
+                x_stream = 0
                 y_stream = 0
-  
+
                 if x > 1 && x < SIZE && y > 1 && y < SIZE
                     for k in 1:9
                         @inbounds x_stream = x - cx[k]
                         @inbounds y_stream = y - cy[k]
-                        ind =  ( k - 1 ) * SIZE * SIZE + x * SIZE + y
-                        iind = ( k - 1 ) * SIZE * SIZE + x_stream * SIZE + y_stream
-                        @inbounds f[trunc(Int,ind)] = f1[trunc(Int,iind)] 
+                        ind = (k - 1) * SIZE * SIZE + x * SIZE + y
+                        iind = (k - 1) * SIZE * SIZE + x_stream * SIZE + y_stream
+                        @inbounds f[trunc(Int, ind)] = f1[trunc(Int, iind)]
                     end
                     for k in 1:9
-                        ind =  ( k - 1 ) * SIZE * SIZE + x * SIZE + y
-                        @inbounds p = p[1,1] + f[ind]
-                        @inbounds u = u[1,1] + f[ind] * cx[k]
-                        @inbounds v = v[1,1] + f[ind] * cy[k]
+                        ind = (k - 1) * SIZE * SIZE + x * SIZE + y
+                        @inbounds p = p[1, 1] + f[ind]
+                        @inbounds u = u[1, 1] + f[ind] * cx[k]
+                        @inbounds v = v[1, 1] + f[ind] * cy[k]
                     end
                     u = u / p
                     v = v / p
                     for k in 1:9
-                        @inbounds cu = cx[k] * u + cy[k] * v 
-                        @inbounds feq = w[k] * p * ( 1.0 + 3.0 * cu + cu * cu - 1.5 * ( ( u * u ) + ( v * v ) ) )
-                        ind =  ( k - 1 ) * SIZE * SIZE + x * SIZE + y
-                        @inbounds f2[trunc(Int,ind)] = f[trunc(Int,ind)] * (1.0 - 1.0 / t) + feq * 1 / t
+                        @inbounds cu = cx[k] * u + cy[k] * v
+                        @inbounds feq = w[k] * p * (1.0 + 3.0 * cu + cu * cu - 1.5 * ((u * u) + (v * v)))
+                        ind = (k - 1) * SIZE * SIZE + x * SIZE + y
+                        @inbounds f2[trunc(Int, ind)] = f[trunc(Int, ind)] * (1.0 - 1.0 / t) + feq * 1 / t
                     end
                 end
             end
@@ -217,17 +217,17 @@ end
     w = ones(9)
     t = 1.0
 
-    df  = JACC.Array(f)
+    df = JACC.Array(f)
     df1 = JACC.Array(f1)
     df2 = JACC.Array(f2)
     dcx = JACC.Array(cx)
     dcy = JACC.Array(cy)
-    dw  = JACC.Array(w)
+    dw = JACC.Array(w)
 
-    JACC.parallel_for((SIZE,SIZE),lbm_kernel,df,df1,df2,t,dw,dcx,dcy,SIZE)
+    JACC.parallel_for((SIZE, SIZE), lbm_kernel, df, df1, df2, t, dw, dcx, dcy, SIZE)
 
-    lbm_threads(f,f1,f2,t,w,cx,cy,SIZE)
+    lbm_threads(f, f1, f2, t, w, cx, cy, SIZE)
 
     @test f2 ≈ df2 rtol = 1e-1
- 
+
 end
