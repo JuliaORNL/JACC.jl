dd1343f6874dacb8901a72fd5ca7a67dcbeedac8
William F Godoy <williamfgc@yahoo.com>
2024-02-28 16:37:25 -0500

    Refactor CI and address AMD sync issue
    Add 4 cores in GitHub Actions tests
    Remove performance tests
    Remove AMDGPU sync calls

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,20 +3,12 @@ import JACC
 const backend = JACC.JACCPreferences.backend
 
 @static if backend == "cuda"
-    println("CUDA backend loaded")
+    @show "CUDA backend loaded"
     include("tests_cuda.jl")
-    if "perf" in ARGS
-        println("Running performance tests")
-        include("tests_cuda_perf.jl")
-    end
 
 elseif backend == "amdgpu"
     @show "AMDGPU backend loaded"
     include("tests_amdgpu.jl")
-    if "perf" in ARGS
-        println("Running performance tests")
-        include("tests_amdgpu_perf.jl")
-    end
 
 elseif backend == "oneapi"
     @show "OneAPI backend loaded"
@@ -25,8 +17,5 @@ elseif backend == "oneapi"
 elseif backend == "threads"
     @show "Threads backend loaded"
     include("tests_threads.jl")
-    if "perf" in ARGS
-        println("Running performance tests")
-        include("tests_threads_perf.jl")
-    end
+
 end
