f14e8019d8dffe4fc9770bc83a4e91c43c8010c3
William F Godoy <williamfgc@yahoo.com>
2024-04-09 16:27:59 -0400

    Refactor tests
    Separate weak dependencies back ends

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,16 +1,21 @@
 import JACC
 
+using Pkg
+
 const backend = JACC.JACCPreferences.backend
 
 @static if backend == "cuda"
+    Pkg.add(name="CUDA", version="v5.1.1")
     @show "CUDA backend loaded"
     include("tests_cuda.jl")
 
 elseif backend == "amdgpu"
+    Pkg.add(name="AMDGPU", version="v0.8.6")
     @show "AMDGPU backend loaded"
     include("tests_amdgpu.jl")
 
 elseif backend == "oneapi"
+    Pkg.add("oneAPI")
     @show "OneAPI backend loaded"
     include("tests_oneapi.jl")
 
