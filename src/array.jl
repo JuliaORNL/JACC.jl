8574135c05cf2f8e5b4f660d926e34107eaab530
William F Godoy <williamfgc@yahoo.com>
2024-04-17 17:35:06 -0400

    Support zeros and ones
    JACC maps to the zeros and ones of each back end
    Add tests
    Add array.jl files for array specific function creation

---
+++ src/array.jl
@@ -0,0 +1,17 @@
+
+
+function zeros(T, dims...)
+	return Base.zeros(T, dims...)
+end
+
+function ones(T, dims...)
+	return Base.ones(T, dims...)
+end
+
+function zeros(dims...)
+	return Base.zeros(dims...)
+end
+
+function ones(dims...)
+	return Base.ones(dims...)
+end
