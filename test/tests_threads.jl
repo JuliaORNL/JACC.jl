be2cec1bcd236e565f98f48fbcd8cefdaa2f0c29
Karl Pierce <kmp5@vt.edu>
2024-05-13 15:18:22 -0400

    Small updates to threads test

--- test/tests_threads.jl
+++ test/tests_threads.jl
@@ -38,8 +38,8 @@ end
     y = round.(rand(Float32, N) * 100)
     alpha = 2.5
 
-    x_host_JACC = JACC.Array(x)
-    y_host_JACC = JACC.Array(y)
+    x_host_JACC = JACC.array(x)
+    y_host_JACC = JACC.array(y)
     JACC.parallel_for(N, axpy, alpha, x_host_JACC, y_host_JACC)
 
     x_expected = x
@@ -59,19 +59,20 @@ end
     alpha = 2.5
     counter = zeros(Int32, 1)
 
-    x_device = JACC.Array(round.(rand(Float32, N) * 100))
-    y_device = JACC.Array(round.(rand(Float32, N) * 100))
-    counter = JACC.Array{Int32}([0])
+    x_device = JACC.array(round.(rand(Float32, N) * 100))
+    y_device = JACC.array(round.(rand(Float32, N) * 100))
+    counter = JACC.array(Int32[0])
     JACC.parallel_for(N, axpy_counter!, alpha, x_device, y_device, counter)
 
     @test counter[1] == N
 end
 
 @testset "zeros" begin
+    elt = Float32
     N = 10
-    x = JACC.zeros(Float32, N)
-    @test typeof(x) == Vector{Float32}
-    @test eltype(x) == Float32
+    x = JACC.zeros(elt, N)
+    @test typeof(x) == Vector{elt}
+    @test eltype(x) == elt
     @test zeros(N)≈x rtol=1e-5
 
     function add_one(i, x)
@@ -83,10 +84,11 @@ end
 end
 
 @testset "ones" begin
+    elt = Float64
     N = 10
-    x = JACC.ones(Float64, N)
-    @test typeof(x) == Vector{Float64}
-    @test eltype(x) == Float64
+    x = JACC.ones(elt, N)
+    @test typeof(x) == Vector{elt}
+    @test eltype(x) == elt
     @test ones(N)≈x rtol=1e-5
 
     function minus_one(i, x)
@@ -98,6 +100,7 @@ end
 end
 
 @testset "CG" begin
+    elt = Float64
     function matvecmul(i, a1, a2, a3, x, y, SIZE)
         if i == 1
             y[i] = a2[i] * x[i] + a1[i] * x[i + 1]
@@ -117,15 +120,10 @@ end
     end
 
     SIZE = 10
-    a0 = ones(SIZE)
-    a1 = ones(SIZE)
-    a2 = ones(SIZE)
-    r = ones(SIZE)
-    p = ones(SIZE)
-    s = zeros(SIZE)
-    x = zeros(SIZE)
-    r_old = zeros(SIZE)
-    r_aux = zeros(SIZE)
+    a0, a1, a2, r, p = JACC.ones.(elt, (SIZE, SIZE, SIZE, SIZE, SIZE))
+    
+    s, x, r_old, r_aux = JACC.zeros.(elt, (SIZE, SIZE, SIZE, SIZE))
+
     a1 = a1 * 4
     r = r * 0.5
     p = p * 0.5
@@ -160,6 +158,7 @@ end
 end
 
 @testset "LBM" begin
+    elt = Float64
     function lbm_kernel(x, y, f, f1, f2, t, w, cx, cy, SIZE)
         u = 0.0
         v = 0.0
@@ -237,38 +236,22 @@ end
     end
 
     SIZE = 10
-    f = ones(SIZE * SIZE * 9) .* 2.0
-    f1 = ones(SIZE * SIZE * 9) .* 3.0
-    f2 = ones(SIZE * SIZE * 9) .* 4.0
-    cx = zeros(9)
-    cy = zeros(9)
-    cx[1] = 0
-    cy[1] = 0
-    cx[2] = 1
-    cy[2] = 0
-    cx[3] = -1
-    cy[3] = 0
-    cx[4] = 0
-    cy[4] = 1
-    cx[5] = 0
-    cy[5] = -1
-    cx[6] = 1
-    cy[6] = 1
-    cx[7] = -1
-    cy[7] = 1
-    cx[8] = -1
-    cy[8] = -1
-    cx[9] = 1
-    cy[9] = -1
+    SIZE2 = SIZE * SIZE * 9
+    f = ones(elt, SIZE2) .* 2.0
+    f1 = ones(elt, SIZE2 ) .* 3.0
+    f2 = ones(elt, SIZE2) .* 4.0
+    cx = elt[0.0, 1.0, -1.0, 0.0, 0.0, 1.0, -1.0, -1.0, 1.0]
+    cy = elt[0.0, 0.0, 0.0, 1.0, -1.0, 1.0, 1.0, -1.0, -1.0]
+
     w = ones(9)
     t = 1.0
 
-    df = JACC.Array(f)
-    df1 = JACC.Array(f1)
-    df2 = JACC.Array(f2)
-    dcx = JACC.Array(cx)
-    dcy = JACC.Array(cy)
-    dw = JACC.Array(w)
+    df = JACC.array(f)
+    df1 = JACC.array(f1)
+    df2 = JACC.array(f2)
+    dcx = JACC.array(cx)
+    dcy = JACC.array(cy)
+    dw = JACC.array(w)
 
     JACC.parallel_for(
         (SIZE, SIZE), lbm_kernel, df, df1, df2, t, dw, dcx, dcy, SIZE)
