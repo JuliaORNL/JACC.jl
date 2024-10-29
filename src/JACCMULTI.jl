c327dcaf29737804349f9c212b41ab9584079629
pedrovalerolara <valerolarap@ornl.com>
2024-10-29 12:10:39 -0400

    Added stencil-aware funtions for JACC.multi on CUDA back ends. There are still a couple of things to implement, such as the ghost copy functions for 2D arrays as well as capability for 3D Arrays

--- src/JACCMULTI.jl
+++ src/JACCMULTI.jl
@@ -9,6 +9,21 @@ function Array(x::Base.Array{T,N}) where {T,N}
   return x
 end
 
+function gArray(x::Base.Array{T,N}) where {T,N}
+end
+
+function gid(dev_id::I, i::I, ndev::I) where{I <: Integer}
+end
+
+function gswap(x::Vector{Any})
+end
+
+function gcopytoarray(x::Vector{Any}, y::Vector{Any})
+end
+
+function copytogarray(x::Vector{Any}, y::Vector{Any})
+end
+
 function copy(x::Vector{Any}, y::Vector{Any})
 end
 
