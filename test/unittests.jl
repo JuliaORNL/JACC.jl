d9ece10a9339d28aadee4fa0b61d8c500f838784
Philip Fackler <facklerpw@ornl.gov>
2024-11-21 12:10:28 -0600

    Updated JACC.BLAS test

--- test/unittests.jl
+++ test/unittests.jl
@@ -125,12 +125,6 @@ end
 end
 
 @testitem "JACC.BLAS" begin
-    x = ones(1_000)
-    y = ones(1_000)
-    jx = JACC.ones(1_000)
-    jy = JACC.ones(1_000)
-    alpha = 2.0
-
     function seq_axpy(N, alpha, x, y)
         for i in 1:N
             @inbounds x[i] += alpha * y[i]
@@ -145,6 +139,12 @@ end
         return r
     end
 
+    x = ones(1_000)
+    y = ones(1_000)
+    jx = JACC.ones(1_000)
+    jy = JACC.ones(1_000)
+    alpha = 2.0
+
     seq_axpy(1_000, alpha, x, y)
     ref_result = seq_dot(1_000, x, y)
 
@@ -152,6 +152,75 @@ end
     jresult = JACC.BLAS.dot(1_000, jx, jy)
 
     @test jresult≈ref_result rtol=1e-8
+
+    x = ones(1_000)
+    y = ones(1_000)
+    y1 = y*2
+    jx = JACC.ones(1_000)
+    jy = JACC.ones(1_000)
+    jy1 = jy*2
+    alpha = 2.0
+  
+    function seq_scal(N, alpha, x)
+        for i in 1:N
+            @inbounds x[i] = alpha * x[i]
+        end
+    end
+ 
+    function seq_asum(N, x)
+        r = 0.0
+        for i in 1:N
+            @inbounds r += abs(x[i])
+        end
+        return r
+    end
+ 
+    function seq_nrm2(N, x)
+        sum_sq = 0.0
+        for i in 1:N
+            @inbounds sum_sq += x[i]*x[i]
+        end
+        r = sqrt(sum_sq)
+        return r
+    end
+ 
+    function seq_swap(N, x, y1)
+        for i in 1:N
+            @inbounds t = x[i]
+            @inbounds x[i] = y1[i]
+            @inbounds y1[i] = t
+        end       
+    end
+
+    ref_result = seq_axpy(1_000, alpha, x, y)
+    ref_result = seq_dot(1_000, x, y)   
+    JACC.BLAS.axpy(1_000, alpha, jx, jy)
+    jresult = JACC.BLAS.dot(1_000, jx, jy)
+    @test jresult≈ref_result rtol=1e-8
+
+    seq_scal(1_000, alpha, x)
+    JACC.BLAS.scal(1_000, alpha, jx)
+    @test x≈Core.Array(jx) rtol=1e-8 
+
+    seq_axpy(1_000, alpha, x, y)
+    JACC.BLAS.axpy(1_000, alpha, jx, jy)
+    @test x≈Core.Array(jx) atol=1e-8
+
+    r1 = seq_dot(1_000, x, y) 
+    r2 = JACC.BLAS.dot(1_000, jx, jy)
+    @test r1≈r2 atol=1e-8 
+
+    r1 = seq_asum(1_000, x)
+    r2 = JACC.BLAS.asum(1_000, jx)
+    @test r1≈r2 atol=1e-8 
+    r1 = seq_nrm2(1_000, x)
+    r2 = JACC.BLAS.nrm2(1_000, jx)
+    @test r1≈r2 atol=1e-8 
+
+    seq_swap(1_000, x, y1)  
+    JACC.BLAS.swap(1_000, jx, jy1)
+    @test x == Core.Array(jx)
+    @test y1 == Core.Array(jy1)
 end
 
 @testitem "Add-2D" begin
@@ -187,68 +256,6 @@ end
 
     C_expected = Float32(2.0) .* ones(Float32, L, M, N)
     @test Core.Array(C)≈C_expected rtol=1e-5
-
-    function seq_scal(N, alpha, x)
-        for i in 1:N
-            @inbounds x[i] = alpha * x[i]
-        end
-    end
-
-    function seq_asum(N, x)
-        r = 0.0
-        for i in 1:N
-            @inbounds r += abs(x[i])
-        end
-        return r
-    end
-
-    function seq_nrm2(N, x)
-        sum_sq = 0.0
-        for i in 1:N
-            @inbounds sum_sq += x[i] * x[i]
-        end
-        r = sqrt(sum_sq)
-        return r
-    end
-
-    function seq_swap(N, x, y1)
-        for i in 1:N
-            @inbounds t = x[i]
-            @inbounds x[i] = y1[i]
-            @inbounds y1[i] = t
-        end
-    end
-
-    # Comparing JACC.BLAS with regular seuential functions
-    # seq_axpy(1_000, alpha, x, y)
-    # ref_result = seq_dot(1_000, x, y)  
-    # JACC.BLAS.axpy(1_000, alpha, jx, jy)
-    # jresult = JACC.BLAS.dot(1_000, jx, jy)
-    # result = Array(jresult)        
-    # @test result[1]≈ref_result rtol=1e-8
-
-    # seq_scal(1_000, alpha, x)
-    # JACC.BLAS.scal(1_000, alpha, jx)
-    # @test x≈Array(jx) atol=1e-8 
-
-    # seq_axpy(1_000, alpha, x, y)
-    # JACC.BLAS.axpy(1_000, alpha, jx, jy)
-    # @test x≈Array(jx) atol=1e-8
-
-    # r1 = seq_dot(1_000, x, y) 
-    # r2 = JACC.BLAS.dot(1_000, jx, jy)
-    # @test r1≈Array(r2)[1] atol=1e-8 
-
-    # r1 = seq_asum(1_000, x)
-    # r2 = JACC.BLAS.asum(1_000, jx)
-    # r1 = seq_nrm2(1_000, x)
-    # r2 = JACC.BLAS.nrm2(1_000, jx)
-    # @test r1≈Array(r2)[1] atol=1e-8
-
-    #seq_swap(1_000, x, y1)  
-    #JACC.BLAS.swap(1_000, jx, jy1)
-    #@test x == Array(jx)
-    #@test y1 == Array(jy1)
 end
 
 @testitem "CG" begin
