863caa396c303a777e378bfe51f831088fa516d1
Philip Fackler <facklerpw@ornl.gov>
2024-12-18 11:30:24 -0600

    Add @init_backend for convenience based on chosen backend

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,29 +1,5 @@
 import JACC
-
-using Pkg
-
-const backend = JACC.JACCPreferences.backend
-
-@static if backend == "cuda"
-    # Pkg.add(; name = "CUDA", version = "v5.1.1")
-    Pkg.add("CUDA")
-    @info "CUDA backend loaded"
-    using CUDA
-
-elseif backend == "amdgpu"
-    Pkg.add(; name = "AMDGPU", version = "v0.8.6")
-    # Pkg.add("AMDGPU")
-    @info "AMDGPU backend loaded"
-    using AMDGPU
-
-elseif backend == "oneapi"
-    Pkg.add("oneAPI")
-    @info "oneAPI backend loaded"
-    using oneAPI
-
-elseif backend == "threads"
-    @info "Threads backend loaded"
-end
+JACC.@init_backend
 
 using ReTest
 include("JACCTests.jl")
