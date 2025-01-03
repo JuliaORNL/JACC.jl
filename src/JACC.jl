7d783406c92052e257974251aa066b0b8951e54b
Philip Fackler <facklerpw@ornl.gov>
2025-01-03 13:37:00 -0600

    Added interface functions for simplified reduce cases

--- src/JACC.jl
+++ src/JACC.jl
@@ -129,22 +129,41 @@ function parallel_for((L, M, N)::Tuple{I, I, I}, f::F,
     return parallel_for(default_backend(), (L, M, N), f, x...)
 end
 
+default_init(::Type{T}, ::typeof(+)) where {T} = zero(T)
+default_init(::Type{T}, ::typeof(*)) where {T} = one(T)
+default_init(::Type{T}, ::typeof(max)) where{T} = typemin(T)
+default_init(::Type{T}, ::typeof(min)) where{T} = typemax(T)
+default_init(op::Function) = default_init(default_float(), op)
+
 function parallel_reduce(
-        N::I, op, f::F, x...; init) where {I <: Integer, F <: Function}
+        N::I, op::Function, f::Function, x...; init) where {I <: Integer}
     return parallel_reduce(default_backend(), N, op, f, x...; init = init)
 end
 
-function parallel_reduce(N::Integer, f::Function, x...)
-    return parallel_reduce(N, +, f, x...; init = zero(default_float()))
-end
-
-function parallel_reduce((M, N)::Tuple{I, I}, op, f::F, x...;
-        init) where {I <: Integer, F <: Function}
+function parallel_reduce((M, N)::Tuple{I, I}, op::Function, f::Function, x...;
+        init) where {I <: Integer}
     return parallel_reduce(default_backend(), (M, N), op, f, x...; init = init)
 end
 
-function parallel_reduce((M, N)::Tuple{Integer, Integer}, f::Function, x...)
-    return parallel_reduce((M, N), +, f, x...; init = zero(default_float()))
+function parallel_reduce(N::Integer, f::Function, x...)
+    return parallel_reduce(N, +, f, x...; init = default_init(+))
 end
+
+function parallel_reduce((M, N)::Tuple{Integer, Integer}, f::Function, x...)
+    return parallel_reduce((M, N), +, f, x...; init = default_init(+))
+end
+
+array_size(a::AbstractArray) = size(a)
+array_size(a::AbstractVector) = length(a)
+
+elem_access(a::AbstractArray) = (i,j,k,a) -> a[i,j,k]
+elem_access(a::AbstractMatrix) = (i,j,a) -> a[i,j]
+elem_access(a::AbstractVector) = (i,a) -> a[i]
+
+function parallel_reduce(op::Function, a::AbstractArray; init = default_init(eltype(a), op))
+    return parallel_reduce(array_size(a), op, elem_access(a), a; init = init)
+end
+
+parallel_reduce(a::AbstractArray; kw...) = parallel_reduce(+, a; kw...)
 
 end # module JACC
