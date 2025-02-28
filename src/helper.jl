6a4c28a76b0df082d0f295ad47701efce8498fdd
Philip Fackler <facklerpw@ornl.gov>
2025-02-28 13:12:10 -0600

    Install backend on set_backend

--- src/helper.jl
+++ src/helper.jl
@@ -1,4 +1,3 @@
-
 function _maybe_threaded(ex)
     quote
         if Threads.nthreads() == 1
@@ -15,30 +14,16 @@ end
 
 function _init_backend()
     quote
-        using Pkg
-        deps = Pkg.project().dependencies
         const backend = JACC.JACCPreferences.backend
         if backend == "cuda"
-            if !haskey(deps, "CUDA")
-                Pkg.add("CUDA")
-                @info "Added CUDA (be careful about committing Project.toml)"
-            end
             using CUDA
             @info "CUDA backend loaded"
 
         elseif backend == "amdgpu"
-            if !haskey(deps, "AMDGPU")
-                Pkg.add("AMDGPU")
-                @info "Added AMDGPU (be careful about committing Project.toml)"
-            end
             using AMDGPU
             @info "AMDGPU backend loaded"
 
         elseif backend == "oneapi"
-            if !haskey(deps, "oneAPI")
-                Pkg.add("oneAPI")
-                @info "Added oneAPI (be careful about committing Project.toml)"
-            end
             using oneAPI
             @info "oneAPI backend loaded"
 
